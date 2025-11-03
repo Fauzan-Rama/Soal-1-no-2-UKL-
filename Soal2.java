fdf5408f51bd4485b922d629c991e1548d31545a
Fauzan-Rama <fauzanramadhan3676@gmail.com>
2025-11-04 06:31:50 +0700

    Add files via upload

---
+++ Soal2.java
@@ -0,0 +1,24 @@
+package Soal1;
+
+import java.util.Scanner;
+
+public class Soal2 {
+    public static void main(String[] args) {
+        Scanner masukan = new Scanner(System.in);
+        int bilangan1;
+        System.out.println("\nCEK BILANGAN GANJIL / GENAP");
+        System.out.print("Masukkan Bilangan : ");
+        bilangan1 = masukan.nextInt();
+        
+        if(bilangan1 % 2 == 0) {
+            System.out.println("Bilangan Genap");
+            System.out.println("Bilangan ini bisa dibagi 2");
+        } else {
+            System.out.println("Bilangan Ganjil");
+            System.out.println("Bilangan ini tidak bisa dibagi 2");
+            System.out.println("Karena hasil bagi = sisa 1");
+        }
+        
+        masukan.close();
+    }
+}
